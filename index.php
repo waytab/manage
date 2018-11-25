d4265c5b561aa17e50b3d7f71839581dcc593178
Andrew Ward <tmiandreww@gmail.com>
2018-11-25 16:02:43 -0500

    home and signin pages

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Manage</title>
+  </head>
+  <body>
+    <div class="container mt-5">
+      <h1>Manage</h1>
+      <p class="lead">Manage is WayTab's remote management suite</p>
+      <p>Manage allows us to implement features like special bell schedules.</p>
+      <p><a href="https://waytab.org" class="btn btn-primary">WayTab.org</a> <a href="signin.php" class="btn btn-primary">Sign in</a></p>
+    </div>
+  </body>
+</html>
---
+++ signin.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Sign in - Manage</title>
+
+    <link rel="stylesheet" href="css/signin.css">
+  </head>
+  <body class="text-center">
+    <form class="form-signin">
+      <h1 class="display-3 mb-3">Manage</h1>
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" name="email">
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="" name="password">
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    </form>
+  </body>
+</html>
